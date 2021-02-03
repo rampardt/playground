24f1c344e4ade83904e146fee40b49b57904b5bb
dsmith <dsmith@dawnsedge.com>
2021-02-03 17:15:49 -0500

    wip

--- app/components/destinations.js
+++ app/components/destinations.js
@@ -1,4 +1,29 @@
 import Component from '@glimmer/component';
+import {tracked} from "@glimmer/tracking";
+
+
+/**
+ * @argument {array} destinations
+ */
+
+class Record {
+  @tracked isEdit = false;
+
+  data;
+}
 
 export default class DestinationsComponent extends Component {
+
+  get records() {
+    return this.args.destinations?.map(item => {
+      let record = new Record();
+      record.data = item;
+      return record;
+    })
+  }
+
+  rollback(record) {
+    record.data.rollbackAttributes();
+
+  }
 }
